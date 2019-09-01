907ed743bf27e99b8bed3cd22a6fe09bf98c9144
José Carneiro <josecarneiro@Joses-MacBook-Pro.local>
2019-09-01 16:20:42 +0100

    Fix broken color for career development schedule

--- src/util/day.js
+++ src/util/day.js
@@ -12,6 +12,3 @@ export const calculateDateFromDay = day => ({
   day: ((day - 1) % 5) + 1,
   week: Math.floor((day - 1) / 5) + 1
 });
-
-// // 1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 19 20 21 22
-// // 1  2  3  4  5  5  5  6  7  8  9  10 10 10 11 12 13 14 15 15 15 16
